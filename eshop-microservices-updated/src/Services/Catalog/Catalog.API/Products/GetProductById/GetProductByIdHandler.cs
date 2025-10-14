ef03a758a65edb7392d88231d4df64646b22c80c
CedrickMemel <cedrickmemel@gmail.com>
2025-10-14 09:43:22 +0200

    Product vertical architecture implementation : *CreateProduct *GetProducts *GetProductById

---
+++ eshop-microservices-updated/src/Services/Catalog/Catalog.API/Products/GetProductById/GetProductByIdHandler.cs
@@ -0,0 +1,22 @@
+﻿using Catalog.API.Exceptions;
+
+namespace Catalog.API.Products.GetProductById;
+
+public record GetProductByIdQuery(Guid Id) : IQuery<GetProductByIdResult>;
+
+public record GetProductByIdResult(Product Product);
+internal class GetProductByIdQueryHandler(IDocumentSession sesion, ILogger<GetProductByIdQueryHandler> logger) : IQueryHandler<GetProductByIdQuery, GetProductByIdResult>
+{
+    public async Task<GetProductByIdResult> Handle(GetProductByIdQuery query, CancellationToken cancellationToken)
+    {
+        logger.LogInformation("GetProductByIdQueryHandler.Handle called with {@Query}", query);
+        var result = await sesion.LoadAsync<Product>(query.Id, cancellationToken);
+
+        if (result is null)
+        {
+            throw new ProductNotFoundException();
+        }
+
+        return new GetProductByIdResult(result);
+    }
+}
---
+++ eshop-microservices-updated/src/Services/Catalog/Catalog.API/Products/GetProducts/GetProductsEndPoint.cs
@@ -0,0 +1,22 @@
+﻿namespace Catalog.API.Products.GetProducts;
+
+//public record GetProductsREquest();
+
+public record GetProductsResponse(IEnumerable<Product> Products);
+
+public class GetProductsEndPoint : ICarterModule
+{
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app.MapGet("/products", async (ISender sender) =>
+        {
+            var result = await sender.Send(new GetProductsQuery());
+            var response = result.Adapt<GetProductsResponse>();
+            return Results.Ok(response);
+        }).WithName("GetProducts")
+        .WithDescription("Get Products")
+        .Produces<GetProductsResponse>(StatusCodes.Status200OK)
+        .ProducesProblem(StatusCodes.Status400BadRequest)
+        .WithSummary("Get Products");
+    }
+}
